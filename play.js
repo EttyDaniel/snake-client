e69eef79cedf396c654f81f932710b5d161671b3
Eti Daniel <danieleti@gmail.com>
2021-08-17 20:41:01 -0400

    adding client and modifying play

--- play.js
+++ play.js
@@ -1,24 +1,6 @@
 //This will serve as our main file through which we will launch the game client.
 const net = require('net');
 
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: 'localhost',
-    port: 50541
-
-  });
-  //receives a message from the server and presents it on the screen
-  conn.on('data', (data) => {
-    console.log('Server says: ', data);
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-
-  return conn;
-}
-
+const  connect  = require('./client');
 console.log('Connecting ...');
 connect();
