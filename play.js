1f339a159e9a3ee4794dbe7e16e9af42d628ae7a
Eti Daniel <danieleti@gmail.com>
2021-08-19 22:08:29 -0400

    moving setupInput to input file

--- play.js
+++ play.js
@@ -1,30 +1,10 @@
 //This will serve as our main file through which we will launch the game client.
-const net = require('net');
 
+const net = require('net');
+const {setupInput} = require('./input');
 const  connect  = require('./client');
+
 console.log('Connecting ...');
 connect();
-
-const handleUserInput = function(key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
-
-/**
- * Setup User Interface 
- * Specifically, so that we can handle user input via stdin
- */
- const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data', (key) => {
-    handleUserInput(key);
-  });
-
-  return stdin;
-};
 
 setupInput();
